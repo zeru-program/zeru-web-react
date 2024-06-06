7603a1ec590919da64304457d3624166dead4521
zeru-program <justinebogor0609@gmail.com>
2024-06-06 15:05:48 +0700

    add our source code page fix

--- src/pages/landing-page/Home.tsx
+++ src/pages/landing-page/Home.tsx
@@ -474,6 +474,57 @@ const [myProject, setMyProject] = useState<Project[]>([]);
     )
 }
 
+const SourceCode = () => {
+    
+    const [result, setResult] = useState<any[]>([]);
+    
+    useEffect(() => {
+          (async () => {
+            // ...
+            fetch(urlDb + 'sourceCode.json')
+            .then(res => res.json())
+            .then(data => {
+              var array: any[] = [];
+              for (let key in data) {
+                array.push(data[key])
+              }
+              setResult(array)
+            })
+          })();
+     }, [])
+    
+    const handleClickBox = (id: any) => {
+        var idR = encodeURIComponent(id)
+        window.location.href = "/source-code/" + idR
+    }
+    
+    return (
+     <>
+     <main className="py-5 mt-5">
+      <h1 className="text-center m-0 fw-bold">Our source code.</h1>
+      <p className="text-center m-0" style={{color:"gray"}}>want to try my creativity?</p>
+      <div className="mt-3 gap-4 justify-content-center d-flex flex-wrap container">
+      
+      {  result.map((data, index) => ( 
+        <div key={index} className="d-flex overflow-hidden flex-column align-items-center rounded-3 rounded" style={{width:"350px",height:"300px", borderRadius:"5px",boxShadow:"0 0 10px rgba(0,0,0,.3)",wordBreak: "break-word"}} onClick={() => handleClickBox(`${data.title}`)}>
+          <img src="/bgcode.jpg" alt="source code default image" style={{width:"100%"}} />
+          <div className="container mt-2">
+           <p className="fw-bold m-0">{data.title}</p>
+           <p className="" style={{fontSize:"14px"}}>
+             {data.description.length > 100? 
+          `${data.description.substring(0, 100)}...` : 
+          data.description}
+           </p>
+          </div>
+        </div>
+      ))}
+        
+      </div>
+     </main>
+     </>
+   )
+}
+
 const ContactUs = () => {
     
     const [iptNama, setIptNama] = useState('')
@@ -670,6 +721,7 @@ const ContentHome: React.FC<ContentHomeProps> = ({
       <Service />
       <Portofolio />
       <Project />
+      <SourceCode />
       <ContactUs />
  {/* <ins className="adsbygoogle" style={{"display":"inline-block","width":"360px","height":"800px"}} data-ad-client="ca-pub-2052442865900021" data-ad-slot={3410580948} /> */}
      </main>
