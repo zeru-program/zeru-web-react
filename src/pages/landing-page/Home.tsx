d58b1892bb92116aeee9152032ff3da7cdbd8bd4
zeru-program <justinebogor0609@gmail.com>
2024-06-07 09:35:15 +0700

    upd 3.0.4 ui

--- src/pages/landing-page/Home.tsx
+++ src/pages/landing-page/Home.tsx
@@ -1,4 +1,6 @@
 import React, { useState, useEffect } from 'react'
+import Navbar from "./Navbar"
+import Footer from "./Footer"
 import '../../App.css'
 import '../../Responsive.css'
 
@@ -73,88 +75,6 @@ const handleBtt = (e: React.MouseEvent<HTMLButtonElement>): void => {
 }
 
 
-const HeaderNavbar = () => {
-    
-     const [navLinks, setNavLinks] = useState('')
-     const [imgNav, setImgNav] = useState('menu.png')
-     const [navShadow, setNavShadow] = useState("")
-    var valueBar: boolean = false
-
-    // clicked hamburger
-    function changeImgNav() {
-    if (valueBar) {
-        setImgNav("menu.png")
-        setNavLinks("");
-    } else {
-         setImgNav("cross.png")
-         setNavLinks("act-link");
-    }
-    valueBar = !valueBar;
-}
-
-// shadow nav
-useEffect(() => {
-  const handleScroll = () => {
-    if (window.pageYOffset >= (document.getElementById('home')?.offsetHeight || 0) - (document.getElementById('nav')?.offsetHeight || 0)) {
-      setNavShadow('nav-shadow');
-    } else {
-      setNavShadow('');
-    }
-  };
-
-  window.addEventListener('scroll', handleScroll);
-
-  // Clean up the event listener when the component unmounts
-  return () => {
-    window.removeEventListener('scroll', handleScroll);
-  };
-}, []);
-    
-    const isLog = localStorage.getItem("hasLogin")
-    const user = localStorage.getItem("username")
-
-    
-    const logout = () => {
-    localStorage.removeItem('hasLogin');
-    localStorage.removeItem('username');
-    localStorage.removeItem('email');
-    localStorage.removeItem('password');
-  location.reload();
-    }
-    
-    return (
-        <>
-        {/* =============== Header & Navbar ================ */}
-<header>
-  <nav>
-    <div className={`con-nav ${navShadow}`}>
-      <label className="logo-nav"><a className="text-decoration-none" href="/"><img src="/favicon.png" alt='logo' style={{width:"40px"}} /></a></label>
-      <img className="bar-nav" onClick={changeImgNav} src={imgNav} alt="nav menu" />
-    </div>
-    <div className={`nav-links ${navLinks}`}>
-      <a id="toHome" className="link-item" href="/">Home</a>
-      <a id="toAbout" href="/#about" className="link-item">About us</a>
-      <a id="toSkils" href="/#skils" className="link-item">Skils</a>
-      <a id="toPortofolio" href="/#portofolio" className="link-item">Portofolio n Project</a>
-      <a id="toContact" href="/#contact" className="link-item">Contact</a>
-     {!isLog && (
-      <>
-        <a id="sign-in-btn" onClick={() => window.location.href = '/sign-in'} className="link-item p-2 btn btn-outline-primary">Sign-in</a>
-        <a id="sign-up-btn" onClick={() => window.location.href = '/sign-up'} className="link-item p-2 btn btn-bg-primary btn-outline-primary text-light">Sign-up</a>
-      </>
-    )}
-    {isLog && (
-    <>
-      <a id="textUserNav" style={{marginLeft :"10px"}} className="d-flex p-2">Hello  {user}!</a>
-      <a id="logout" className="d-flex link-item p-2 btn-outline-danger" style={{marginLeft :"10px"}} onClick={logout} >Log-out</a>
-      </>
-      )}
-    </div>
-  </nav>
-</header>
-        </>
-    )
-}
 
 const Homepage: React.FC<HomepageProps> = ({
   logoTr,
@@ -199,10 +119,16 @@ const Homepage: React.FC<HomepageProps> = ({
       <button className={`${btnTalkTrClass} btn-talk`} style={{ animation: `${btnTalkTr}` }} onClick={() => window.location.href="/#contact"}>Let's Talk</button>
       <button className={`${btnCvTrClass} btn-cv`} style={{ paddingInline: "8px !important", animation: `${btnCvTr}` }} onClick={handleCv}>Download CV</button>
     </div>
-    <div className={`${conImgTalkTrClass} ccon-img-tal`} style={{ animation: `${conImgTalkTr}` }}>
-     <img src="whatsapp.png" onClick={() => window.location.href = 'https://api.whatsapp.com/send?phone=6287774487198'} className="img-talk-social" alt="" />
-    <img src="instagram.png" onClick={() => window.location.href = 'https://www.instagram.com/zerr.ace/'} className="img-talk-social" alt="" />
-    <img src="gmail.png" onClick={() => window.location.href = 'mailto:zeruprogram@gmail.com?subject=HiZeru!'} className="img-talk-social" alt="" />
+    <div className={`${conImgTalkTrClass} con-img-tal`} style={{ animation: `${conImgTalkTr}`, display: "flex" }}>
+     <div className="img-talk-con">
+     <i onClick={() => window.location.href = 'https://api.whatsapp.com/send?phone=6287774487198'} className="img-talk-social bi bi-whatsapp"></i>
+     </div>
+     <div className="img-talk-con">
+    <i onClick={() => window.location.href = 'https://www.instagram.com/zerr.ace/'} className="img-talk-social bi bi-instagram"></i>
+     </div>
+     <div className="img-talk-con">
+    <i onClick={() => window.location.href = 'mailto:zeruprogram@gmail.com?subject=HiZeru!'} className="img-talk-social bi bi-envelope"></i>
+     </div>
     </div>
   </div>
   <svg className="waves-home" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
@@ -581,23 +507,23 @@ const ContactUs = () => {
       <br />
       <div className="isi-info-contact">
         <div className="c1-contact" style={{paddingLeft:"5px"}}>
-          <i className="img-talk-social bi bi-github" />
+          <i className="img-contact bi bi-github" />
           <label><a href="https://github.com/zeru-program/" className="href-contact">@zeru-program</a></label>
         </div>
         <div className="c1-contact">
-          <img src="whatsapp.png" className="img-talk-social" alt="" />
+          <img src="whatsapp.png" className="img-contact" alt="" />
           <label><a href="https://wa.me/6289526333265" className="href-contact">+6289526333265</a></label>
         </div>
         <div className="c1-contact">
-          <img src="instagram.png" className="img-talk-social" alt="" />
+          <img src="instagram.png" className="img-contact" alt="" />
           <label><a href="https://www.instagram.com/zerr.ace?r" className="href-contact">@zerr.ace</a></label>
         </div>
         <div className="c1-contact">
-          <img src="gmail.png" className="img-talk-social" alt='' />
+          <img src="gmail.png" className="img-contact" alt='' />
           <label><a href="mailto:zeruprogram@gmail.com?subject=HireYou" className="href-contact">zeruprogram@gmail.com</a></label>
         </div>
         <div className="c1-contact">
-          <img src="location.png" className="img-talk-social" alt="" />
+          <img src="location.png" className="img-contact" alt="" />
           <label><a href="https://maps.app.goo.gl/DFANiWSdinNK3yhB7" className="href-contact address">   Cipaku, bogor selatan Jawa barat, Indonesia.</a></label>
         </div>
       </div>
@@ -626,55 +552,6 @@ const ContactUs = () => {
     </div>
   </div>
   {/* contact us  page end */}
-        </>
-   )
-}
-
-const Footer = () => {
-    return (
-        <>
-        {/* footer start */}
-<footer className="">
-  <div className="container-footer">
-    <label className="logo-footer">Zerr.</label>
-    <div className="isi-f">
-      <label className="title-footer">Information web zeru</label>
-      <p className="sub-footer">Created by zeru</p>
-      <p className="sub-footer">programming language Javascript + Typescript</p>
-      <p className="sub-footer">framework use, bootstrap, React js + typescript</p>
-      <p className="sub-footer">library database from firebase </p>
-      <p className="sub-footer">library css ANIMATE.CSS,TRANSITION.CSS</p>
-      <p className="sub-footer">hosting firebase and github</p>
-      <p className="sub-footer">created in 17 febuary 2024</p>
-    </div>
-    <div className="isi-f">
-      <label className="title-footer">Services</label>
-      <p className="sub-footer">web developer</p>
-      <p className="sub-footer">web design / UI-UX</p>
-      <p className="sub-footer">front-end web engineer</p>
-      <p className="sub-footer">back-end web engineer</p>
-      <p className="sub-footer">designer</p>
-      <p className="sub-footer">cinematography</p>
-    </div>
-    <div className="isi-f">
-      <label className="title-footer">Activity</label>
-    {/*  <p className="sub-footer">study in smpn school</p>*/}
-      <p className="sub-footer">study programing otodidak</p>
-      <p className="sub-footer">freelance (friend's project)</p>
-      <p className="sub-footer">church service</p>
-      <p className="sub-footer">photography</p>
-    </div>
-    <div className="isi-f">
-      <label className="title-footer">Social media zeru</label>
-      <p className="sub-footer"><a href="https://github.com/zeru-program/">Github</a></p>
-      <p className="sub-footer"><a href="https://wa.me/6289526333265">WhatsApp</a></p>
-      <p className="sub-footer"><a href="https://www.instagram.com/zerr.ace?r">Instagram</a></p>
-      <p className="sub-footer"><a href="mailto:zeruprogram@gmail.com?subject=HireYou">Email</a></p>
-    </div>
-  </div>
-  <label className="copyright mt-3">Copyright ©2024 all rights reserved. by zeru, version web app 3.0.0</label>
-</footer>
-{/* footer end */}
         </>
    )
 }
@@ -837,7 +714,7 @@ if (aboutElement && typeof aboutElement.offsetTop === 'number') {
   return (
       <>
       <BtnBackToTop handleScrollUp={handleScrollUp} />
-      <HeaderNavbar />
+      <Navbar />
       <ContentHome  logoTr={logoTr} titleTr={titleTr} subTr={subTr} btnTalkTr={btnTalkTr} btnCvTr={btnCvTr} conImgTalkTr={conImgTalkTr} logoTrClass={logoTrClass} titleTrClass={titleTrClass} subTrClass={subTrClass} btnTalkTrClass={btnTalkTrClass} btnCvTrClass={btnCvTrClass} conImgTalkTrClass={conImgTalkTrClass} aboutTitleTr={aboutTitleTr} aboutTitleTrClass={aboutTitleTrClass} aboutSubtitleTrClass={aboutSubtitleTrClass} />
       <Footer />
       <Modals />
--- src/pages/sc/SourceCode.tsx
+++ src/pages/sc/SourceCode.tsx
@@ -1,88 +1,7 @@
 import { useState, useEffect } from "react"
 let urlDb = 'https://zerupgmm-default-rtdb.firebaseio.com/';
-
-const HeaderNavbar = () => {
-    
-     const [navLinks, setNavLinks] = useState('')
-     const [imgNav, setImgNav] = useState('menu.png')
-     const [navShadow, setNavShadow] = useState("")
-    var valueBar: boolean = false
-
-    // clicked hamburger
-    function changeImgNav() {
-    if (valueBar) {
-        setImgNav("menu.png")
-        setNavLinks("");
-    } else {
-         setImgNav("cross.png")
-         setNavLinks("act-link");
-    }
-    valueBar = !valueBar;
-}
-
-// shadow nav
-useEffect(() => {
-  const handleScroll = () => {
-    if (window.pageYOffset >= (document.getElementById('home')?.offsetHeight || 0) - (document.getElementById('nav')?.offsetHeight || 0)) {
-      setNavShadow('nav-shadow');
-    } else {
-      setNavShadow('');
-    }
-  };
-
-  window.addEventListener('scroll', handleScroll);
-
-  // Clean up the event listener when the component unmounts
-  return () => {
-    window.removeEventListener('scroll', handleScroll);
-  };
-}, []);
-    
-    const isLog = localStorage.getItem("hasLogin")
-    const user = localStorage.getItem("username")
-
-    
-    const logout = () => {
-    localStorage.removeItem('hasLogin');
-    localStorage.removeItem('username');
-    localStorage.removeItem('email');
-    localStorage.removeItem('password');
-  location.reload();
-    }
-    
-    return (
-        <>
-        {/* =============== Header & Navbar ================ */}
-<header>
-  <nav>
-    <div className={`con-nav ${navShadow}`}>
-      <label className="logo-nav"><a className="text-decoration-none" href="/"><img src="/favicon.png" alt='logo' style={{width:"40px"}} /></a></label>
-      <img className="bar-nav" onClick={changeImgNav} src={imgNav} alt="nav menu" />
-    </div>
-    <div className={`nav-links ${navLinks}`}>
-      <a id="toHome" className="link-item" href="/">Home</a>
-      <a id="toAbout" href="/#about" className="link-item">About us</a>
-      <a id="toSkils" href="/#skils" className="link-item">Skils</a>
-      <a id="toPortofolio" href="/#portofolio" className="link-item">Portofolio n Project</a>
-      <a id="toContact" href="/#contact" className="link-item">Contact</a>
-     {!isLog && (
-      <>
-        <a id="sign-in-btn" onClick={() => window.location.href = '/sign-in'} className="link-item p-2 btn btn-outline-primary">Sign-in</a>
-        <a id="sign-up-btn" onClick={() => window.location.href = '/sign-up'} className="link-item p-2 btn btn-bg-primary btn-outline-primary text-light">Sign-up</a>
-      </>
-    )}
-    {isLog && (
-    <>
-      <a id="textUserNav" style={{marginLeft :"10px"}} className="d-flex p-2">Hello  {user}!</a>
-      <a id="logout" className="d-flex link-item p-2 btn-outline-danger" style={{marginLeft :"10px"}} onClick={logout} >Log-out</a>
-      </>
-      )}
-    </div>
-  </nav>
-</header>
-        </>
-    )
-}
+import Navbar from "../landing-page/Navbar"
+import Footer from "../landing-page/Footer"
 
 const Content = () => {
     
@@ -134,61 +53,10 @@ const Content = () => {
    )
 }
 
-
-const Footer = () => {
-    return (
-        <>
-        {/* footer start */}
-<footer className="">
-  <div className="container-footer">
-    <label className="logo-footer">Zerr.</label>
-    <div className="isi-f">
-      <label className="title-footer">Information web zeru</label>
-      <p className="sub-footer">Created by zeru</p>
-      <p className="sub-footer">programming language Javascript + Typescript</p>
-      <p className="sub-footer">framework use, bootstrap, React js + typescript</p>
-      <p className="sub-footer">library database from firebase </p>
-      <p className="sub-footer">library css ANIMATE.CSS,TRANSITION.CSS</p>
-      <p className="sub-footer">hosting firebase and github</p>
-      <p className="sub-footer">created in 17 febuary 2024</p>
-    </div>
-    <div className="isi-f">
-      <label className="title-footer">Services</label>
-      <p className="sub-footer">web developer</p>
-      <p className="sub-footer">web design / UI-UX</p>
-      <p className="sub-footer">front-end web engineer</p>
-      <p className="sub-footer">back-end web engineer</p>
-      <p className="sub-footer">designer</p>
-      <p className="sub-footer">cinematography</p>
-    </div>
-    <div className="isi-f">
-      <label className="title-footer">Activity</label>
-    {/*  <p className="sub-footer">study in smpn school</p>*/}
-      <p className="sub-footer">study programing otodidak</p>
-      <p className="sub-footer">freelance (friend's project)</p>
-      <p className="sub-footer">church service</p>
-      <p className="sub-footer">photography</p>
-    </div>
-    <div className="isi-f">
-      <label className="title-footer">Social media zeru</label>
-      <p className="sub-footer"><a href="https://github.com/zeru-program/">Github</a></p>
-      <p className="sub-footer"><a href="https://wa.me/6289526333265">WhatsApp</a></p>
-      <p className="sub-footer"><a href="https://www.instagram.com/zerr.ace?r">Instagram</a></p>
-      <p className="sub-footer"><a href="mailto:zeruprogram@gmail.com?subject=HireYou">Email</a></p>
-    </div>
-  </div>
-  <label className="copyright mt-3">Copyright ©2024 all rights reserved. by zeru, version web app 3.0.0</label>
-</footer>
-{/* footer end */}
-        </>
-   )
-}
-
-
 const SourceCode = () => {
     return (
         <>
-     <HeaderNavbar/>
+     <Navbar/>
      <Content/>
      <Footer/>
      </>
